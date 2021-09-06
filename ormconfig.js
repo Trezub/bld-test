50b6c4d19991bd097d292ab897e93154df64629a
Adriano Trezub Déa <adriano.trezub@gmail.com>
2021-09-05 22:33:20 -0300

    Updated ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,43 +1,34 @@
-const { SnakeNamingStrategy } = require('typeorm-naming-strategies');
-
-module.exports = process.env.NODE_ENV === 'production' ? {
-    namingStrategy: new SnakeNamingStrategy(),
-    type: process.env.DB_CONNECTION,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    logging: false,
-    synchronize: false,
-    entities: [
-        'dist/models/**/*.js',
-    ],
-    migrations: [
-        'dist/migrations/**/*.js',
-    ],
-    cli: {
-        entitiesDir: 'dist/models',
-        migrationsDir: 'dist/migrations',
-    },
-} : {
-    namingStrategy: new SnakeNamingStrategy(),
-    type: process.env.DB_CONNECTION,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    logging: false,
-    synchronize: true,
-    entities: [
-        'src/models/**/*.ts',
-    ],
-    migrations: [
-        'src/migrations/**/*.ts',
-    ],
-    cli: {
-        entitiesDir: 'src/models',
-        migrationsDir: 'src/migrations',
-    },
-};
+module.exports =
+    process.env.NODE_ENV === 'production'
+        ? {
+              type: process.env.DB_CONNECTION,
+              host: process.env.DB_HOST,
+              port: process.env.DB_PORT,
+              username: process.env.DB_USERNAME,
+              password: process.env.DB_PASSWORD,
+              database: process.env.DB_DATABASE,
+              logging: false,
+              synchronize: false,
+              entities: ['dist/models/**/*.js'],
+              migrations: ['dist/migrations/**/*.js'],
+              cli: {
+                  entitiesDir: 'dist/models',
+                  migrationsDir: 'dist/migrations',
+              },
+          }
+        : {
+              type: process.env.DB_CONNECTION,
+              host: process.env.DB_HOST,
+              port: process.env.DB_PORT,
+              username: process.env.DB_USERNAME,
+              password: process.env.DB_PASSWORD,
+              database: process.env.DB_DATABASE,
+              logging: false,
+              synchronize: false,
+              entities: ['src/models/**/*.ts'],
+              migrations: ['src/migrations/**/*.ts'],
+              cli: {
+                  entitiesDir: 'src/models',
+                  migrationsDir: 'src/migrations',
+              },
+          };
